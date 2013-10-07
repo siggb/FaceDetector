0ff50ffc30b30e7aa73f5968a586c1157ab0d70c
Ildar Sibagatov <sibagatovildar@MacBook-Pro-Ildar.local>
2013-10-07 23:02:33 +0400

    - project files reorganization

---
+++ Sources/Pages/MainScreen/MainScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainScreenViewController.h
+//  FaceDetector
+//
+//  Created by Ildar Sibagatov on 07.10.13.
+//  Copyright (c) 2013 Sig Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainScreenViewController : UIViewController
+
+@end
---
+++ Sources/Pages/MainScreen/MainScreenViewController.m
@@ -0,0 +1,38 @@
+//
+//  MainScreenViewController.m
+//  FaceDetector
+//
+//  Created by Ildar Sibagatov on 07.10.13.
+//  Copyright (c) 2013 Sig Inc. All rights reserved.
+//
+
+#import "MainScreenViewController.h"
+
+@interface MainScreenViewController ()
+
+@end
+
+@implementation MainScreenViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
